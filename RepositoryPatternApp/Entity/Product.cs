fd2d16286db21ea95ca953753af7dac91c761ce7
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2024-02-17 07:05:43 +0300

    Initial commit

---
+++ RepositoryPatternApp/Entity/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace RepositoryPatternApp.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
